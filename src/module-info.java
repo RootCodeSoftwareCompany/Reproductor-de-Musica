79485cc8f1f193c121d895779a5a6f817dbdb8e8
RootCodeSoftwareCompany <rootcode.softwarecompany@gmail.com>
2025-03-26 20:32:49 -0400

    Se agregro el ultimo proyecto enviado por Alvaro

---
+++ src/module-info.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module Reproductordemusica {
+    requires javafx.swt;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires java.base;
+    opens reproductordemusica to javafx.fxml;
+    exports reproductordemusica;
+}
---
+++ src/reproductordemusica/ReproductordeMusica.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package reproductordemusica;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+public class ReproductordeMusica extends Application {
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Reproductor de Musica");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void cambiarEscena(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader(ReproductordeMusica.class.getResource(fxml));
+        Parent root = loader.load();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
